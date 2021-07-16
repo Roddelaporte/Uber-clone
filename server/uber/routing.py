6c9572d2c21fbdf3918aaa28358c209371210f57
Rodrigo Delaporte <rod.delaporte@gmail.com>
2021-07-15 20:55:28 -0400

    test Websocket passed

--- server/uber/routing.py
+++ server/uber/routing.py
@@ -1,7 +1,13 @@
 from django.core.asgi import get_asgi_application
+from django.urls import path
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from trips.consumers import UberConsumer
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
+    'websocket': URLRouter([
+        path('taxi/', UberConsumer.as_asgi()),
+    ]),
 })
