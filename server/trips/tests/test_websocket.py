362b3bcca1559d22c4582eaae625e3fe298c2bae
Rodrigo Delaporte <rod.delaporte@gmail.com>
2021-07-16 20:53:19 -0400

    Test Sending and Recieving Broadcast Messages

--- server/trips/tests/test_websocket.py
+++ server/trips/tests/test_websocket.py
@@ -1,5 +1,6 @@
 import pytest
 from channels.testing import WebsocketCommunicator
+from channels.layers import get_channel_layer
 
 from uber.routing import application
 
@@ -23,7 +24,6 @@ class TestWebSocket:
         assert connected is True
         await communicator.disconnect()
 
-
     async def test_can_send_and_receive_messages(self, settings):
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
         communicator = WebsocketCommunicator(
@@ -36,6 +36,23 @@ class TestWebSocket:
             'data': 'This is a test message.',
         }
         await communicator.send_json_to(message)
+        response = await communicator.receive_json_from()
+        assert response == message
+        await communicator.disconnect()
+    
+    async def test_can_send_and_receive_broadcast_messages(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/uber/'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message.',
+        }
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send('test', message=message)
         response = await communicator.receive_json_from()
         assert response == message
         await communicator.disconnect()
