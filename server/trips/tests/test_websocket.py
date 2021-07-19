197cbfbfcd96c6cc754104fd7c875ec5d6b54386
Rodrigo Delaporte <rod.delaporte@gmail.com>
2021-07-19 04:07:19 -0400

    Uncomment Broadcast Test

--- server/trips/tests/test_websocket.py
+++ server/trips/tests/test_websocket.py
@@ -62,25 +62,25 @@ class TestWebSocket:
         assert response == message
         await communicator.disconnect()
     
-    # async def test_can_send_and_receive_broadcast_messages(self, settings):
-    #     settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
-    #     _, access = await create_user(
-    #         'test.user@example.com', 'pAssw0rd'
-    #     )
-    #     communicator = WebsocketCommunicator(
-    #         application=application,
-    #         path=f'/uber/?token={access}'
-    #     )
-    #     connected, _ = await communicator.connect()
-    #     message = {
-    #         'type': 'echo.message',
-    #         'data': 'This is a test message.',
-    #     }
-    #     channel_layer = get_channel_layer()
-    #     await channel_layer.group_send('test', message=message)
-    #     response = await communicator.receive_json_from()
-    #     assert response == message
-    #     await communicator.disconnect()
+    async def test_can_send_and_receive_broadcast_messages(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        _, access = await create_user(
+            'test.user@example.com', 'pAssw0rd'
+        )
+        communicator = WebsocketCommunicator(
+            application=application,
+            path=f'/uber/?token={access}'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message.',
+        }
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send('test', message=message)
+        response = await communicator.receive_json_from()
+        assert response == message
+        await communicator.disconnect()
     
     async def test_cannot_connect_to_socket(self, settings):
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
