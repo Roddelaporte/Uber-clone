347a1abca910e49108a20dc1baf2dbdb8b9ad9b2
Rodrigo Delaporte <rod.delaporte@gmail.com>
2021-07-19 21:53:04 -0400

    User Photo

--- server/trips/tests/test_http.py
+++ server/trips/tests/test_http.py
@@ -1,4 +1,7 @@
 from django.contrib.auth import get_user_model
+from io import BytesIO
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
@@ -20,8 +23,16 @@ def create_user(username='user@example.com', password=PASSWORD, group_name='ride
     return user
 
 
+def create_photo_file():
+    data = BytesIO()
+    Image.new('RGB', (100, 100)).save(data, 'PNG')
+    data.seek(0)
+    return SimpleUploadedFile('photo.png', data.getvalue())
+
+
 class AuthenticationTest(APITestCase):
     def test_user_can_sign_up(self):
+        photo_file = create_photo_file()
         response = self.client.post(reverse('sign_up'), data={
             'username': 'user@example.com',
             'first_name': 'Test',
@@ -29,6 +40,7 @@ class AuthenticationTest(APITestCase):
             'password1': PASSWORD,
             'password2': PASSWORD,
             'group': 'rider',
+            'photo': photo_file,
         })
         user = get_user_model().objects.last()
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
@@ -37,6 +49,7 @@ class AuthenticationTest(APITestCase):
         self.assertEqual(response.data['first_name'], user.first_name)
         self.assertEqual(response.data['last_name'], user.last_name)
         self.assertEqual(response.data['group'], user.group)
+        self.assertIsNotNone(user.photo)
     
 
     def test_user_can_log_in(self):
